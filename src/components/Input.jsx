1497738ebd5fb18d888b3e34f35427c8b54568d1
Margus Lehiste <hr.lehiste@gmail.com>
2024-03-04 20:01:23 -0500

    Content Picks tab

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,8 +12,10 @@ export default function Input({ label, invalid, ...props }) {
 
   return (
     <p>
-      <label className={labelClasses}>{label}</label>
-      <input className={inputClasses} {...props} />
+      <label className='block text-xs font-bold tracking-wide uppercase text-stone-300'>
+        {label}</label>
+      <input className='w-full px-3 py-2 leading-tight border rounded shadow text-gray-700 bg-stone-100' 
+        {...props} />
     </p>
   );
 }
