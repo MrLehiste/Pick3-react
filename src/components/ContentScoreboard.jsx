b843d68bbd93d87b5a8e191c99bc58291c22cfc0
Margus Lehiste <hr.lehiste@gmail.com>
2024-04-27 15:00:04 -0400

    days dot plot fix

--- src/components/ContentScoreboard.jsx
+++ src/components/ContentScoreboard.jsx
@@ -163,7 +163,6 @@ export default function Scoreboard({ state }) {
       </tfoot>
     </table>);
     if(tab == SCORE_TABS[1]) {
-      const days31 = Array.from({ length: 31 }, (_, i) => i + 1);
       resultsTable = (<table className="block min-w-full divide-y divide-gray-300 border-separate border-spacing-0">
         <thead className="bg-gray-50">
           <tr>
@@ -230,53 +229,56 @@ export default function Scoreboard({ state }) {
         </th>
       </tr>
     </tfoot>) : "";
-    if(tab == SCORE_TABS[2]) resultsTable = (<table className="min-w-full border-separate border-spacing-0">
-    <thead>
-      <tr>
-        <th scope="col" className="rounded-tl-lg sticky top-0 left-0 z-9 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+    if(tab == SCORE_TABS[2]) {
+      const days31 = Array.from({ length: 31 }, (_, i) => i + 1);
+      resultsTable = (<table className="min-w-full border-separate border-spacing-0">
+      <thead>
+        <tr>
+          <th scope="col" className="rounded-tl-lg sticky top-0 left-0 z-9 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
 
-        </th>
-        {HEADER_MONTHS.map((month) => {
-        return(
-        <th key={month.name} scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-center items-center justify-center text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter">
-          {month.name}
-        </th>
-        )})}
-        <th scope="col" className="rounded-tr-lg sticky top-0 left-0 z-9 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
-
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      {days31.map((d, index) => (
-        <tr key={'dot-tr-'+d+index}>
-          <td className='sticky left-0 z-11 border-b border-gray-200 bg-white whitespace-nowrap py-1 pl-3 pr-3 text-sm font-medium text-gray-900 sm:pl-3 lg:pl-3'>
-            {d}
-          </td>
+          </th>
           {HEADER_MONTHS.map((month) => {
-            return(
-            <td key={'dot-td1-'+month.name+'-'+d} className={classNames('bg-white', 'border-b border-l border-gray-200 whitespace-nowrap py-1 pl-1 pr-1 text-xs font-bold text-gray-900 sm:pl-1 lg:pl-1')}>
-              <span className="flex items-center">
-                {qData.filter(x => new Date(x.Dtm).getDate()==d && new Date(x.Dtm).getMonth()==month.number-1)
-                .map((x, i) => (
-                  <span key={'dot-span-q-'+d+index+x.Dtm+i} className={classNames(Q_MAP.filter(m => m.q1 == x.Q11)[0].bg, "w-7 h-7 flex items-center justify-center rounded-full text-black font-bold text-xs shadow-md")}>
-                  {x.Q1}</span>
-                ))}
-              </span>
-            </td>
+          return(
+          <th key={month.name} scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-center items-center justify-center text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter">
+            {month.name}
+          </th>
           )})}
-          <td className='border-b border-l border-gray-200 bg-white whitespace-nowrap py-1 pl-3 pr-3 text-sm font-medium text-gray-900 sm:pl-3 lg:pl-3'>
-          <div className='grid place-items-center'>
-            <div className="bg-gray-800 w-7 h-7 flex items-center justify-center rounded-full text-white font-bold text-base shadow-md">
-              {qData.filter(x => new Date(x.Dtm).getDate()==d).length}
-            </div>
-          </div>
-          </td>
+          <th scope="col" className="rounded-tr-lg sticky top-0 left-0 z-9 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+
+          </th>
         </tr>
-      ))}
-    </tbody>
-    {dotPlotFooter}
-    </table>);
+      </thead>
+      <tbody>
+        {days31.map((d, index) => (
+          <tr key={'dot-tr-'+d+index}>
+            <td className='sticky left-0 z-11 border-b border-gray-200 bg-white whitespace-nowrap py-1 pl-3 pr-3 text-sm font-medium text-gray-900 sm:pl-3 lg:pl-3'>
+              {d}
+            </td>
+            {HEADER_MONTHS.map((month) => {
+              return(
+              <td key={'dot-td1-'+month.name+'-'+d} className={classNames('bg-white', 'border-b border-l border-gray-200 whitespace-nowrap py-1 pl-1 pr-1 text-xs font-bold text-gray-900 sm:pl-1 lg:pl-1')}>
+                <span className="flex items-center">
+                  {qData.filter(x => new Date(x.Dtm).getDate()==d && new Date(x.Dtm).getMonth()==month.number-1)
+                  .map((x, i) => (
+                    <span key={'dot-span-q-'+d+index+x.Dtm+i} className={classNames(Q_MAP.filter(m => m.q1 == x.Q11)[0].bg, "w-7 h-7 flex items-center justify-center rounded-full text-black font-bold text-xs shadow-md")}>
+                    {x.Q1}</span>
+                  ))}
+                </span>
+              </td>
+            )})}
+            <td className='border-b border-l border-gray-200 bg-white whitespace-nowrap py-1 pl-3 pr-3 text-sm font-medium text-gray-900 sm:pl-3 lg:pl-3'>
+            <div className='grid place-items-center'>
+              <div className="bg-gray-800 w-7 h-7 flex items-center justify-center rounded-full text-white font-bold text-base shadow-md">
+                {qData.filter(x => new Date(x.Dtm).getDate()==d).length}
+              </div>
+            </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      {dotPlotFooter}
+      </table>);
+    }
     if(tab == SCORE_TABS[3]) resultsTable = (<table className="min-w-full border-separate border-spacing-0">
     <thead>
       <tr>
