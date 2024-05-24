e9cd717057bd6f19df72bb8752f2642f155a5670
Margus Lehiste <hr.lehiste@gmail.com>
2024-05-24 17:40:08 -0400

    picks lines bold ballcolor

--- src/components/ContentPicks.jsx
+++ src/components/ContentPicks.jsx
@@ -6,7 +6,7 @@ import LoadingIndicator from './UI/LoadingIndicator.jsx';
 import ErrorBlock from './UI/ErrorBlock.jsx';
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css';
-import { DATE_OPT_MDY4, DATE_OPT_DAY_LONG, DATE_OPT_MONTH_LONG, classNames } from './UI/constants';
+import { DATE_OPT_MDY4, DATE_OPT_DAY_LONG, DATE_OPT_MONTH_LONG, Q_MAP, classNames } from './UI/constants';
 
 const MONTH_0 = new Date(new Date().setMonth(new Date().getMonth() - 1));
 const DAY_1 = new Date();
@@ -19,14 +19,19 @@ const PICKS_TABS = [
   , "Tomorrow's Picks"
   , MONTH_1.toLocaleDateString('en-US', DATE_OPT_MONTH_LONG)+"' Picks"
   , MONTH_2.toLocaleDateString('en-US', DATE_OPT_MONTH_LONG)+"' Picks"];
+const STR_MY_TAB = "My Tab";
+const STR_PICKS_TAB = "picks-tab";
+const STR_2_PICKS = "2. Picks";
+const STR_PICKS_DTFROM = "picks-dtfrom";
+const STR_PICKS_DTTO = "picks-dtto";
 
 export default function ContentPicks({ state, onPageChange }) {
   const [tab, setTab] = useState(PICKS_TABS[0]);
   const handleTabChange = (value) => { 
     setTab(value); 
     console.log("TAB", value);
-    localStorage.setItem('picks-tab', value); 
-    onPageChange("2. Picks" ); 
+    localStorage.setItem(STR_PICKS_TAB, value); 
+    onPageChange(STR_2_PICKS); 
     initDates(value);
   };
   const initDates = (value) => {
@@ -47,8 +52,9 @@ export default function ContentPicks({ state, onPageChange }) {
         setDtTo(DAY_2.toLocaleDateString('en-US', DATE_OPT_MDY4));
         break;
       case PICKS_TABS[3]:
-        MONTH_1.setDate(1);
-        setDtFrom(MONTH_1.toLocaleDateString('en-US', DATE_OPT_MDY4));
+        //MONTH_1.setDate(1);
+        //setDtFrom(MONTH_1.toLocaleDateString('en-US', DATE_OPT_MDY4));
+        setDtFrom(DAY_1.toLocaleDateString('en-US', DATE_OPT_MDY4));
         MONTH_1.setMonth(MONTH_1.getMonth() + 1);
         MONTH_1.setDate(0);
         setDtTo(MONTH_1.toLocaleDateString('en-US', DATE_OPT_MDY4));
@@ -63,19 +69,38 @@ export default function ContentPicks({ state, onPageChange }) {
     }
   }
   useEffect(() => {
-    const storedTab = localStorage.getItem('picks-tab');
+    const storedTab = localStorage.getItem(STR_PICKS_TAB);
     if (storedTab && storedTab !== tab) { 
       setTab(storedTab); 
-      onPageChange("2. Picks" ); 
-      initDates(storedTab);
+      onPageChange(STR_2_PICKS); 
+      if(storedTab==STR_MY_TAB){
+        const storedDtFrom = localStorage.getItem(STR_PICKS_DTFROM);
+        if(storedDtFrom) setDtFrom(storedDtFrom);
+        const storedDtTo = localStorage.getItem(STR_PICKS_DTTO);
+        if(storedDtTo) setDtTo(storedDtTo);
+      } 
+      else { initDates(storedTab); } 
+      
     }
     return () => {};
   }, []);
 
   const [dtFrom, setDtFrom] = useState(new Date().toLocaleDateString('en-US', DATE_OPT_MDY4));
-  const handleFromChange = (date) => { setDtFrom(new Date(date).toLocaleDateString('en-US', DATE_OPT_MDY4)); };
+  const handleFromChange = (date) => { 
+    const dtfrom = new Date(date).toLocaleDateString('en-US', DATE_OPT_MDY4); 
+    setDtFrom(dtfrom);
+    localStorage.setItem(STR_PICKS_DTFROM, dtfrom); 
+    setTab(STR_MY_TAB);
+    localStorage.setItem(STR_PICKS_TAB, STR_MY_TAB);
+  };
   const [dtTo, setDtTo] = useState(new Date().toLocaleDateString('en-US', DATE_OPT_MDY4));
-  const handleToChange = (date) => { setDtTo(new Date(date).toLocaleDateString('en-US', DATE_OPT_MDY4)); };
+  const handleToChange = (date) => { 
+    const dtto = new Date(date).toLocaleDateString('en-US', DATE_OPT_MDY4); 
+    setDtTo(dtto); 
+    localStorage.setItem(STR_PICKS_DTTO, dtto); 
+    setTab(STR_MY_TAB);
+    localStorage.setItem(STR_PICKS_TAB, STR_MY_TAB);
+  };
 
   const picksUrl = import.meta.env.VITE_URL_PICKS + '&state='+state+'&dt1='+dtFrom+'&dt2='+dtTo;
   const { data, isPending, isError, error } = useQuery({
@@ -94,11 +119,11 @@ export default function ContentPicks({ state, onPageChange }) {
   }
   if (data) {
     resultsTable = (<table className="min-w-full divide-y divide-gray-300">
-    <thead className="bg-gray-50">
+    <thead className="sticky top-0 z-10 bg-gray-50">
       <tr>
         <th scope="col" colSpan={2} className="rounded-tl-lg py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
           <span className="flex items-center">
-            <span className="bg-gray-900 w-6 h-6 flex items-center justify-center rounded-full text-white font-bold text-xs shadow-md">
+            <span className="bg-gray-900 w-8 h-8 flex items-center justify-center rounded-full text-white font-bold text-lg shadow-md">
               {data.length}
             </span> 
             <span className='ml-1'>Picks</span>
@@ -124,9 +149,9 @@ export default function ContentPicks({ state, onPageChange }) {
         </th>
       </tr>
     </thead>
-    <tbody className="divide-y divide-gray-200 bg-white">
+    <tbody className="divide-y divide-gray-900 bg-white">
       {data.map((d, index) => (
-        <tr key={"picks-tr-"+index}>
+        <tr key={"picks-tr-"+index+d.Num+d.Dt}>
           <td className={classNames( (index==data.length-1) ? "rounded-bl-lg " : "", "whitespace-nowrap py-1 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6")}>
             {d.Num} 
           </td>
@@ -143,10 +168,14 @@ export default function ContentPicks({ state, onPageChange }) {
           <td className="whitespace-nowrap px-3 py-1 text-sm text-gray-500">{d.Hitnum}</td>
           <td className="whitespace-nowrap px-3 py-1 text-sm text-gray-500">
             {d.Hitnum && <span className='pick-box p-1'>
-              {new Date(d.Hitdtm).toLocaleDateString('en-US', DATE_OPT_MDY4)}</span>}</td>
+              {new Date(d.Hitdtm).toLocaleDateString('en-US', DATE_OPT_MDY4)}</span>}
+            {d.Q && <span className={classNames( Q_MAP.filter(q => q.q1 == d.Q11)[0].bg , "w-8 h-8 flex items-center justify-center rounded-full text-white font-bold text-xs shadow-md")}>
+              {d.Q}
+            </span>}
+          </td>
           <td className="whitespace-nowrap px-3 py-1 text-sm text-gray-500">
-            <span className={d.Magic ? "magic-box pr-1 pl-1" : d.Sq3 ? "trident-box pr-1 pl-1" : ""}>
-              {d.Q} {d.Num} {d.Squiggly}
+            <span className={classNames(d.Magic ? "magic-box pr-1 pl-1" : d.Sq3 ? "trident-box pr-1 pl-1" : "", "text-black font-bold")}>
+             {d.Q} {d.Num} {d.Squiggly}
             </span>
           </td>
           <td className={classNames( (index==data.length-1) ? "rounded-br-lg " : "", "whitespace-nowrap px-3 py-1 text-sm text-gray-500")}>
