29ed4dd11b7b61b5f52bad0e9c0e804fb1501afc
Margus Lehiste <hr.lehiste@gmail.com>
2024-06-08 18:51:41 -0400

    picks magic switch

--- src/components/ContentPicks.jsx
+++ src/components/ContentPicks.jsx
@@ -7,6 +7,7 @@ import ErrorBlock from './UI/ErrorBlock.jsx';
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css';
 import { DATE_OPT_MDY4, DATE_OPT_DAY_LONG, DATE_OPT_MONTH_LONG, Q_MAP, classNames } from './UI/constants';
+import { Field, Label, Switch } from '@headlessui/react'
 
 const MONTH_0 = new Date(new Date().setMonth(new Date().getMonth() - 1));
 const DAY_1 = new Date();
@@ -101,6 +102,8 @@ export default function ContentPicks({ state, onPageChange }) {
     setTab(STR_MY_TAB);
     localStorage.setItem(STR_PICKS_TAB, STR_MY_TAB);
   };
+  const [allToggle, setAllToggle] = useState(false);
+  const handleAllToggle = () => { setAllToggle(prevAllToggle => !prevAllToggle); };
 
   const picksUrl = import.meta.env.VITE_URL_PICKS + '&state='+state+'&dt1='+dtFrom+'&dt2='+dtTo;
   const { data, isPending, isError, error } = useQuery({
@@ -122,13 +125,14 @@ export default function ContentPicks({ state, onPageChange }) {
     );
   }
   if (data) {
+    const pdata = data.filter(d => !allToggle || d.Magic || d.Sq3 );
     resultsTable = (<table className="min-w-full divide-y divide-gray-300">
     <thead className="sticky top-0 z-10 bg-gray-50 text-lg">
       <tr>
         <th scope="col" colSpan={2} className="rounded-tl-lg py-3.5 pl-4 pr-3 text-left font-semibold text-gray-900 sm:pl-6">
           <span className="flex items-center">
             <span className="bg-gray-900 w-8 h-8 flex items-center justify-center rounded-full text-white font-bold text-lg shadow-md">
-              {data.length}
+              {pdata.length}
             </span> 
             <span className='ml-1'>Picks</span>
           </span>
@@ -154,7 +158,7 @@ export default function ContentPicks({ state, onPageChange }) {
       </tr>
     </thead>
     <tbody className="divide-y divide-gray-900 bg-white text-lg">
-      {data.map((d, index) => (
+      {pdata.map((d, index) => (
         <tr key={"picks-tr-"+index+d.Num+d.Dt} className={`cursor-pointer ${highlightedRow === index ? 'bg-yellow-200' : ''}`}
         onClick={() => handleRowClick(index)}>
           <td className={classNames( (index==data.length-1) ? "rounded-bl-lg " : "", "py-3 whitespace-nowrap pl-4 pr-1 font-medium text-gray-900 sm:pl-6")}>
@@ -174,9 +178,9 @@ export default function ContentPicks({ state, onPageChange }) {
           <td className="whitespace-nowrap px-3 py-1 text-gray-500">
             {d.Hitnum && <span className='pick-box p-1'>
               {new Date(d.Hitdtm).toLocaleDateString('en-US', DATE_OPT_MDY4)}</span>}
-            {d.Q && <span className={classNames( Q_MAP.filter(q => q.q1 == d.Q11)[0].bg , "w-8 h-8 flex items-center justify-center rounded-full text-white font-bold text-xs shadow-md")}>
+            {d.Q ? <span className={classNames( Q_MAP.filter(q => q.q1 == d.Q11)[0].bg , "w-8 h-8 flex items-center justify-center rounded-full text-white font-bold text-xs shadow-md")}>
               {d.Q}
-            </span>}
+            </span> : <span className='m-3 font-bold'>S</span>}
           </td>
           <td className="whitespace-nowrap px-3 py-1 text-gray-500">
             <span className={classNames(d.Magic ? "magic-box pr-1 pl-1" : d.Sq3 ? "trident-box pr-1 pl-1" : "", "text-black font-bold")}>
@@ -215,6 +219,67 @@ export default function ContentPicks({ state, onPageChange }) {
                           From: <DatePicker className='w-28 mr-2' selected={dtFrom} onChange={handleFromChange} />
                           To: <DatePicker className='w-28 mr-2' selected={dtTo} onChange={handleToChange} />
                         </div>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                      <div className='grid place-items-center'>
+                      <Field as="div" className="flex items-center">
+                        <Label as="span" className="m-3 text-sm cursor-pointer" onClick={handleAllToggle}>
+                          <span className="font-medium text-gray-900">ALL</span>{' '}
+                          <span className="text-gray-500">PICKS</span>
+                        </Label>
+                        <Switch
+                          checked={allToggle}
+                          onChange={handleAllToggle}
+                          className={classNames(
+                            allToggle ? 'bg-indigo-600' : 'bg-gray-200',
+                            'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out outline-none ring-2 ring-indigo-600 ring-offset-2'
+                          )}
+                        >
+                          <span className="sr-only">Use setting</span>
+                          <span
+                            className={classNames(
+                              allToggle ? 'translate-x-5' : 'translate-x-0',
+                              'pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                            )}
+                          >
+                            <span
+                              className={classNames(
+                                allToggle ? 'opacity-0 duration-100 ease-out' : 'opacity-100 duration-200 ease-in',
+                                'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity'
+                              )}
+                              aria-hidden="true"
+                            >
+                              <svg className="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
+                                <path
+                                  d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+                                  stroke="currentColor"
+                                  strokeWidth={2}
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                            </span>
+                            <span
+                              className={classNames(
+                                allToggle ? 'opacity-100 duration-200 ease-in' : 'opacity-0 duration-100 ease-out',
+                                'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity'
+                              )}
+                              aria-hidden="true"
+                            >
+                              <svg className="h-3 w-3 text-indigo-600" fill="currentColor" viewBox="0 0 12 12">
+                                <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+                              </svg>
+                            </span>
+                          </span>
+                        </Switch>
+                        <Label as="span" className="ml-3 text-sm cursor-pointer" onClick={handleAllToggle}>
+                          <span className="font-medium text-gray-900">Magic</span>{' '}
+                          <span className="text-gray-500">Trident €</span>
+                        </Label>
+                      </Field>
+                      </div>
                       </th>
                     </tr>
                   </thead>
