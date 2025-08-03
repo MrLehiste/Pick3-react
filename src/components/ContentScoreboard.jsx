8491dfb8cb082b50b77329635710b413f5d0a5bd
Margus Lehiste <hr.lehiste@gmail.com>
2025-08-03 18:41:54 -0400

    Scoreboard 0 last col removed

--- src/components/ContentScoreboard.jsx
+++ src/components/ContentScoreboard.jsx
@@ -118,14 +118,16 @@ export default function ContentScoreboard({ state, onPageChange }) {
           </div>
         </th>
         )})}
-        <th scope="col" className="rounded-br-lg sticky bottom-0 right-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-8 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
-          <div className='grid place-items-center'>
-            <div className="bg-red-600 w-10 h-10 flex items-center justify-center rounded-full text-white font-bold text-lg shadow-md">
-              {qData.length}
+        {tab !== SCORE_TABS[0] && (
+          <th scope="col" className="rounded-br-lg sticky bottom-0 right-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-8 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+            <div className='grid place-items-center'>
+              <div className="bg-red-600 w-10 h-10 flex items-center justify-center rounded-full text-white font-bold text-lg shadow-md">
+                {qData.length}
+              </div>
+              TOTAL
             </div>
-            TOTAL
-          </div>
-        </th>
+          </th>
+        )}
       </tr>
     </tfoot>) : "";
     if(tab == SCORE_TABS[0]) resultsTable = (<table className="min-w-full border-separate border-spacing-0">
@@ -140,9 +142,6 @@ export default function ContentScoreboard({ state, onPageChange }) {
             {month.name}
           </th>
           )})}
-          <th scope="col" className="rounded-tr-lg sticky top-0 left-0 z-9 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
-  
-          </th>
         </tr>
       </thead>
       <tbody>
@@ -186,13 +185,6 @@ export default function ContentScoreboard({ state, onPageChange }) {
                 {/* {filterData.length == 0 && "EMPTY"} */}
               </td>
             )})}
-            <td className='border-b border-l border-gray-800 bg-white whitespace-nowrap py-1 pl-3 pr-3 text-sm font-medium text-gray-900 sm:pl-3 lg:pl-3'>
-              <div className='grid place-items-center'>
-                <div className="bg-gray-800 w-10 h-10 flex items-center justify-center rounded-full text-white font-bold text-lg shadow-md">
-                  {qData.filter(x => new Date(x.Dtm).getFullYear()==year).length}
-                </div>
-              </div>
-            </td>
           </tr>
         ))}
       </tbody>
